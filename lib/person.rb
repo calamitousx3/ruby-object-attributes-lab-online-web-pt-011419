bb3764aa82dc66fa94b98e1a068ec25ddc68d912
Carmen Renee <carmengenpt@gmail.com>
2019-02-07 16:33:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,37 +6,3 @@ class Person
   def name
     @name 
   end 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Dog
-
-  def name=(name)
-    @name = name
-  end
-
-
- def name
-    @name
-  end
-  
- def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-end
